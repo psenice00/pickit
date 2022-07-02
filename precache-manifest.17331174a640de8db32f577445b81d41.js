36ace3e875665975daf276786f043797cc90dacb
psenijak <psenijak@fel.cvut.cz>
2022-07-02 18:15:49 +0200

    Updates

---
+++ precache-manifest.17331174a640de8db32f577445b81d41.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cedca0c043ab714b6c1",
+    "url": "/psenice00/pickit/static/css/main.d7d8002a.chunk.css"
+  },
+  {
+    "revision": "2cedca0c043ab714b6c1",
+    "url": "/psenice00/pickit/static/js/main.87f06b7d.chunk.js"
+  },
+  {
+    "revision": "f0e4c6d56d08e3c444ba",
+    "url": "/psenice00/pickit/static/js/runtime~main.f11e064b.js"
+  },
+  {
+    "revision": "d5965d620077c772df82",
+    "url": "/psenice00/pickit/static/css/2.48c56d1d.chunk.css"
+  },
+  {
+    "revision": "d5965d620077c772df82",
+    "url": "/psenice00/pickit/static/js/2.adbc6e08.chunk.js"
+  },
+  {
+    "revision": "dec293f6ffd1cafcdfc6",
+    "url": "/psenice00/pickit/static/js/3.b6e49e2a.chunk.js"
+  },
+  {
+    "revision": "703e351756a4ea38432aef92d4c06585",
+    "url": "/psenice00/pickit/static/media/Team.703e3517.png"
+  },
+  {
+    "revision": "989a8839fe4c8efc11d61885a3059d13",
+    "url": "/psenice00/pickit/static/media/bottom_bg.989a8839.png"
+  },
+  {
+    "revision": "3d871478ecfdf2baa95000c62be94d28",
+    "url": "/psenice00/pickit/static/media/map.3d871478.png"
+  },
+  {
+    "revision": "c92bfb32f73e842f787512418953a73b",
+    "url": "/psenice00/pickit/static/media/envelope.c92bfb32.png"
+  },
+  {
+    "revision": "0b1d4beb8ed84ae7db22a6308c38e648",
+    "url": "/psenice00/pickit/static/media/snowCap.0b1d4beb.svg"
+  },
+  {
+    "revision": "32fd6a012f3dba800f13783667a45569",
+    "url": "/psenice00/pickit/static/media/tablet.32fd6a01.png"
+  },
+  {
+    "revision": "d2deb45b4e6e4198b95631f09a0f8a27",
+    "url": "/psenice00/pickit/static/media/Fridge.d2deb45b.png"
+  },
+  {
+    "revision": "9334ddd11e60a68ff730667c05f11ad8",
+    "url": "/psenice00/pickit/static/media/howItWorks1.9334ddd1.png"
+  },
+  {
+    "revision": "389510eac2bfb7b6dbfb0132b211987e",
+    "url": "/psenice00/pickit/static/media/howItWorks2.389510ea.png"
+  },
+  {
+    "revision": "52d57914c428b57d3a8ac9ed113ba0fd",
+    "url": "/psenice00/pickit/static/media/howItWorks3.52d57914.png"
+  },
+  {
+    "revision": "3a67408b9334bcd9c4ff4c05f6360cfc",
+    "url": "/psenice00/pickit/index.html"
+  }
+];
